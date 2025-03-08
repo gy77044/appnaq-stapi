18495676895c65440422b8f7f8be6740b0a2b89b
Your Name <youremail@example.com>
2025-03-08 21:41:23 +0530

    resolve conflict

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,6 +2,29 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io",
+            "https://res.cloudinary.com", // Allow images from Cloudinary
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://res.cloudinary.com", // Allow media from Cloudinary
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
